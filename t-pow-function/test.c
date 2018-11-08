6cc20cf2c2b72f6d5f8ee39625fb769078292993
Doru Bercea <gheorghe-teod.bercea@ibm.com>
2018-11-08 17:41:25 -0500

    Add Raja and math function tests.

--- t-pow-function/test.c
+++ t-pow-function/test.c
@@ -3,17 +3,20 @@
 #include <omp.h>
 #include <math.h>
 
+#include "../utilities/check.h"
+#include "../utilities/utilities.h"
+
 int main(void) {
+  check_offloading();
+
   double A = 2.0;
   float B = 2.0;
 
   #pragma omp target map(A,B)
   {
-    A = powi(A, 4);
-    //B = powif(B, 4);
+    A = sqrt(pow(A, B));
   }
 
   printf("%lf\n",A);
-  //printf("%f\n",B);
   return 0;
 }
